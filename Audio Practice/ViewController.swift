dc06c95082d2e5b17fd427494b3927c5d2409d8c
Taehyung Kim <kevin3238@berkeley.edu>
2016-07-08 12:04:06 -0700

    (6/8/2016 - 12PM) Volume Control, Time Control

--- Audio Practice/ViewController.swift
+++ Audio Practice/ViewController.swift
@@ -11,21 +11,40 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    var player: AVAudioPlayer = AVAudioPlayer() //controls music
+    
+    @IBOutlet var musicTitleLabel: UILabel!
+    @IBOutlet var playPauseButtonIcon: UIButton!
     @IBOutlet var musicVolumeSlider: UISlider!
+    @IBOutlet var musicTimeSlider: UISlider!
+    @IBOutlet var volumeIcon: UIImageView!
     
-    @IBAction func playMusicButton(_ sender: UIButton) {
-        player.play()
+    @IBAction func playPauseButton(_ sender: UIButton) {
+        if player.isPlaying {
+            player.pause()
+            playPauseButtonIcon.setImage(#imageLiteral(resourceName: "white play"), for: [])
+        } else {
+            player.play()
+            playPauseButtonIcon.setImage(#imageLiteral(resourceName: "white pause"), for: [])
+        }
     }
-    
-    @IBAction func pauseMusicButton(_ sender: UIButton) {
-        player.pause()
-    }
-    
+
     @IBAction func adjustMusicVolume(_ sender: UISlider) {
         player.volume = musicVolumeSlider.value
+        if musicVolumeSlider.value == 0.0 {
+            volumeIcon.image = #imageLiteral(resourceName: "white mute")
+        } else {
+            volumeIcon.image = #imageLiteral(resourceName: "white sound")
+        }
     }
     
-    var player: AVAudioPlayer = AVAudioPlayer() //controls music
+    @IBAction func musicTime(_ sender: UISlider) {
+        player.currentTime = TimeInterval (musicTimeSlider.value)
+    }
+    
+    func updateMusicTimeSlider() {
+        musicTimeSlider.value = Float (player.currentTime)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,17 +53,23 @@ class ViewController: UIViewController {
         
         do {
             try player = AVAudioPlayer(contentsOf: URL (fileURLWithPath: audioPath))
-            player.play()
+            
+            musicTitleLabel.text = "Tyga - Hijjack"
+            
+            musicTimeSlider.maximumValue = Float(player.duration)
+            musicTimeSlider.value = 0.0
+            
+            musicVolumeSlider.value = player.volume
         } catch {
-            print ("process error here")
+            print ("Music could not be loaded")
         }
+        
+        _ = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateMusicTimeSlider), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
