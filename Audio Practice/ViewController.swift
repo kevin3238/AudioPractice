3e9de29362c3ab002350443f87fd83a66640be73
Taehyung Kim <kevin3238@berkeley.edu>
2016-07-09 18:08:13 -0700

    7/19/2016 6pm

--- Audio Practice/ViewController.swift
+++ Audio Practice/ViewController.swift
@@ -14,9 +14,17 @@ class ViewController: UIViewController {
     var player: AVAudioPlayer = AVAudioPlayer() //controls music
     
     @IBOutlet var musicTitleLabel: UILabel!
+    @IBOutlet var musicRateLabel: UILabel!
+    
+    @IBOutlet var musicCurrentTime: UILabel!
+    @IBOutlet var musicEndTime: UILabel!
+    
     @IBOutlet var playPauseButtonIcon: UIButton!
+    
     @IBOutlet var musicVolumeSlider: UISlider!
     @IBOutlet var musicTimeSlider: UISlider!
+    @IBOutlet var musicRateSlider: UISlider!
+    
     @IBOutlet var volumeIcon: UIImageView!
     
     @IBAction func playPauseButton(_ sender: UIButton) {
@@ -29,6 +37,8 @@ class ViewController: UIViewController {
         }
     }
 
+    //manual control of volume
+    // *** need to change so that it's consistent with the device. max value should be max value of device and change with device sound changes.
     @IBAction func adjustMusicVolume(_ sender: UISlider) {
         player.volume = musicVolumeSlider.value
         if musicVolumeSlider.value == 0.0 {
@@ -38,13 +48,58 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func musicTime(_ sender: UISlider) {
-        player.currentTime = TimeInterval (musicTimeSlider.value)
+    //manual control of music rate
+    // *** need to change string to only display 2 numbers past decimal
+    @IBAction func adjustMusicRate(_ sender: UISlider) {
+        player.rate = musicRateSlider.value
+        musicRateLabel.text = String(musicRateSlider.value)
     }
     
+    //manual control of music time
+    // *** need to display 2 decimals past seconds
+    @IBAction func musicTime(_ sender: UISlider) {
+        player.currentTime = TimeInterval (musicTimeSlider.value)
+        
+        let currentTimeMin:Int = Int(player.currentTime)/60
+        let currentTimeSec:Int = Int(player.currentTime) % 60
+        
+        if currentTimeMin < 10 {
+            if currentTimeSec < 10 {
+                musicCurrentTime.text = "0\(currentTimeMin):0\(currentTimeSec)"
+            } else {
+                musicCurrentTime.text = "0\(currentTimeMin):\(currentTimeSec)"
+            }
+        } else {
+            if currentTimeSec < 10 {
+                musicCurrentTime.text = "\(currentTimeMin):0\(currentTimeSec)"
+            } else {
+                musicCurrentTime.text = "\(currentTimeMin):\(currentTimeSec)"
+            }
+        }
+    }
+    
+    // slider updates as the music progresses
     func updateMusicTimeSlider() {
         musicTimeSlider.value = Float (player.currentTime)
+        
+        let currentTimeMin:Int = Int(player.currentTime)/60
+        let currentTimeSec:Int = Int(player.currentTime) % 60
+        
+        if currentTimeMin < 10 {
+            if currentTimeSec < 10 {
+                musicCurrentTime.text = "0\(currentTimeMin):0\(currentTimeSec)"
+            } else {
+                musicCurrentTime.text = "0\(currentTimeMin):\(currentTimeSec)"
+            }
+        } else {
+            if currentTimeSec < 10 {
+                musicCurrentTime.text = "\(currentTimeMin):0\(currentTimeSec)"
+            } else {
+                musicCurrentTime.text = "\(currentTimeMin):\(currentTimeSec)"
+            }
+        }
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,12 +109,30 @@ class ViewController: UIViewController {
         do {
             try player = AVAudioPlayer(contentsOf: URL (fileURLWithPath: audioPath))
             
-            musicTitleLabel.text = "Tyga - Hijjack"
+            player.enableRate = true
+            
+            musicTitleLabel.text = "Tyga ft. 2 Chainz - Hijack"
+            
+            musicCurrentTime.text = "00:00"
+            
+            let endTimeMin:Int = Int(player.duration)/60
+            let endTimeSec:Int = Int(player.duration) % 60
+            if (endTimeMin < 10) {
+                musicEndTime.text = "0\(endTimeMin):\(endTimeSec)"
+            } else {
+                musicEndTime.text = "\(endTimeMin):\(endTimeSec)"
+            }
             
             musicTimeSlider.maximumValue = Float(player.duration)
             musicTimeSlider.value = 0.0
             
             musicVolumeSlider.value = player.volume
+            
+            musicRateSlider.value = player.rate
+            musicRateSlider.minimumValue = 0.5
+            musicRateSlider.maximumValue = 1.5
+            musicRateLabel.text = String(player.rate)
+            
         } catch {
             print ("Music could not be loaded")
         }
